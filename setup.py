1abc18880a041188e8259bc637c437a4d3780526
mjtnix <mjtnix@5914aa95-5b3a-0410-a3b5-7b719e7fe9b2>
2008-01-24 19:07:49 +0000

    update version number. empty __init__.py file
    git-svn-id: http://freebase-python.googlecode.com/svn/trunk@32 5914aa95-5b3a-0410-a3b5-7b719e7fe9b2

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ except ImportError:
 
 setup(
     name='freebase',
-    version='0.2.2',
+    version='0.2.3',
     author='Nick Thompson',
     author_email='nix@metaweb.com',
     maintainer_email='developers@freebase.com',
