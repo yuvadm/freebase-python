c2c5cbda1c63086459326d415a4f7c8fd372998f
nitromaster101 <nitromaster101@5914aa95-5b3a-0410-a3b5-7b719e7fe9b2>
2009-06-19 17:25:42 +0000

    forgot to update to newest version
    git-svn-id: http://freebase-python.googlecode.com/svn/trunk@110 5914aa95-5b3a-0410-a3b5-7b719e7fe9b2

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ except ImportError:
 
 setup(
     name='freebase',
-    version='0.3',
+    version='0.4',
     author='Nick Thompson',
     author_email='nix@metaweb.com',
     maintainer_email='developers@freebase.com',
