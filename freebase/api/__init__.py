2e38953f290a30e4f47dbd99e6c114ba3d5da31c
nitromaster101 <nitromaster101@5914aa95-5b3a-0410-a3b5-7b719e7fe9b2>
2009-06-26 21:16:57 +0000

    add a list of literal type ids
    git-svn-id: http://freebase-python.googlecode.com/svn/trunk@145 5914aa95-5b3a-0410-a3b5-7b719e7fe9b2

--- freebase/api/__init__.py
+++ freebase/api/__init__.py
@@ -2,3 +2,18 @@
 from session import HTTPMetawebSession, MetawebError, attrdict
 
 from mqlkey import quotekey, unquotekey
+
+LITERAL_TYPE_IDS = set([
+  "/type/int",
+  "/type/float",
+  "/type/boolean",
+  "/type/rawstring",
+  "/type/uri",
+  "/type/text",
+  "/type/datetime",
+  "/type/bytestring",
+  "/type/id",
+  "/type/key",
+  "/type/value",
+  "/type/enumeration"
+])
