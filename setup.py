88b0611c6eca4451699700b4cd3a5c74a59856c3
nitromaster101 <nitromaster101@5914aa95-5b3a-0410-a3b5-7b719e7fe9b2>
2009-07-11 18:04:51 +0000

    sorry, i forgot to check in the 1.0 branch... you can find the code on pypi
    git-svn-id: http://freebase-python.googlecode.com/svn/trunk@189 5914aa95-5b3a-0410-a3b5-7b719e7fe9b2

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ except ImportError:
 
 setup(
     name='freebase',
-    version='0.5',
+    version='1.0',
     author='Nick Thompson',
     author_email='nix@metaweb.com',
     maintainer_email='developers@freebase.com',
