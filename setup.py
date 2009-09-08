b30b191aaf7be0b96543529fa3accf0305bf8355
alecflett <alecflett@5914aa95-5b3a-0410-a3b5-7b719e7fe9b2>
2009-09-08 17:00:57 +0000

    prep for 1.0.3 branch
    git-svn-id: http://freebase-python.googlecode.com/svn/trunk@213 5914aa95-5b3a-0410-a3b5-7b719e7fe9b2

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ except ImportError:
 
 setup(
     name='freebase',
-    version='1.0',
+    version='1.0.3',
     author='Nick Thompson',
     author_email='nix@metaweb.com',
     maintainer_email='developers@freebase.com',
